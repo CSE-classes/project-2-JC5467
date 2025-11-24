1d31044b0d756af19dfe0a1549a80710ac61443c
JC5467 <jchavez190275@gmail.com>
2025-11-23 20:34:20 -0600

    Update trap.c

--- trap.c
+++ trap.c
@@ -13,6 +13,7 @@ struct gatedesc idt[256];
 extern uint vectors[];  // in vectors.S: array of 256 entry pointers
 struct spinlock tickslock;
 uint ticks;
+extern struct proc *proc;
 extern int page_allocator_type;
 int mappages(pde_t *pgdir, void *va, uint size, uint pa, int perm);
 
